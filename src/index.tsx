a81e1a82e35b8d1a3a09599a79fe3b1853f875c7
Chris Xing <Chris_Xing@epam.com>
2022-06-19 10:52:06 +0800

    Thing

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+// Delete me
+export const Thing = () => {
+  console.log('==========================Thing');
+  return <div>the snozzberries taste like snozzberries</div>;
+};
